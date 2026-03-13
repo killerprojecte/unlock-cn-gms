514eb7b0d80668965d7d8dd4d7f4d90bca281dc1
QAQ <81072191+killerprojecte@users.noreply.github.com>
2026-03-13 23:24:58 +0800

    removal codes

--- customize.sh
+++ customize.sh
@@ -3,8 +3,7 @@
 # Function to find the first available XML permission file
 find_origin() {
     for file in \
-        /odm/etc/permissions/com.gnss.bds_preference.xml \
-        /my_heytap/etc/permissions/my_heytap_cn_gms_features.xml; do
+        /odm/etc/permissions/com.gnss.bds_preference.xml; do
         if [ -e "$file" ]; then
             echo "$file"
             return
@@ -14,27 +13,7 @@ find_origin() {
 }
 
 origin=$(find_origin)
-
-if [[ $origin == *my_bigball* ]]; then
-    target=$MODPATH/oplus_google_cn_gms_features.xml
-    echo -e '#!/system/bin/sh\nmount -o ro,bind ${0%/*}/oplus_google_cn_gms_features.xml /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml' > $MODPATH/post-fs-data.sh
-    # echo 'sleep 60; umount /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml' > $MODPATH/service.sh
-elif [[ $origin == *my_product* ]]; then
-    target=$MODPATH/oplus_google_cn_gms_features.xml
-    echo -e '#!/system/bin/sh\nmount -o ro,bind ${0%/*}/oplus_google_cn_gms_features.xml /my_product/etc/permissions/oplus_google_cn_gms_features.xml' > $MODPATH/post-fs-data.sh
-elif [[ $origin == *my_heytap* ]]; then
-    target=$MODPATH/my_heytap_cn_gms_features.xml
-    echo -e '#!/system/bin/sh\nmount -o ro,bind ${0%/*}/my_heytap_cn_gms_features.xml /my_heytap/etc/permissions/my_heytap_cn_gms_features.xml' > $MODPATH/post-fs-data.sh
-    if [[ -e /my_heytap/etc/permissions/my_heytap_cn_features.xml ]]; then
-        echo -e '\nmount -o ro,bind ${0%/*}/my_heytap_cn_features.xml /my_heytap/etc/permissions/my_heytap_cn_features.xml' >> $MODPATH/post-fs-data.sh
-        heytap_cn_features_orgin=/my_heytap/etc/permissions/my_heytap_cn_features.xml
-        heytap_cn_features_target=$MODPATH/my_heytap_cn_features.xml
-    fi
-elif [[ $origin == *system* ]]; then
-    target=$MODPATH$origin
-else
-    target=$MODPATH/system$origin
-fi
+target=$MODPATH$origin
 
 DIR_OF_TARGET=$(dirname $target)
 mkdir -p $DIR_OF_TARGET
@@ -43,11 +22,3 @@ sed -i '/cn.google.services/d' $target
 sed -i '/services_updater/d' $target
 
 ui_print "modify $origin"
-
-if [[ -e "$heytap_cn_features_orgin" ]]; then
-mkdir -p $(dirname $heytap_cn_features_target)
-cp -f $heytap_cn_features_orgin $heytap_cn_features_target
-sed -i '/cn.google.services/d' $heytap_cn_features_target
-sed -i '/services_updater/d' $heytap_cn_features_target
-ui_print "modify $heytap_cn_features_orgin"
-fi
